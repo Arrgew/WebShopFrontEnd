789aee1193553c2d3d1f6e2aec949b8d0dd9615a
Arrgew <90505612+Arrgew@users.noreply.github.com>
2022-02-01 18:44:00 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Item } from './item';
+import { ItemService } from './item.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WebShopFrontEnd';
+export class AppComponent implements OnInit{
+  public items: Item[] = [];
+
+  constructor(private itemService: ItemService){}
+  
+
+
+  ngOnInit(){
+      this.getItems();
+  }
+
+  getItems(): void{
+    this.itemService.getItems().subscribe(
+      (response: Item[]) => {
+        this.items = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+  public onOpenModal(item: Item, mode: string): void{
+    const container = document.getElementById('main-container')
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add'){
+      button.setAttribute('data-target','#addItemModal');
+    }
+    if (mode === 'edit'){
+      button.setAttribute('data-target','#updateItemModal');
+    }
+    if (mode === 'delete'){
+      button.setAttribute('data-target','#deleteItemModal');
+    }
+  }
 }
+
+
